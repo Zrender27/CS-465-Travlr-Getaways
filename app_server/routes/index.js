317cc81cb08a2f1f54c1f68297eaed87936994a6
Zoe Render <zoerender@Mac.attlocal.net>
2026-09-21 12:32:37 -0400

    Complete Module Two MVC and HBS refactor

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var ctrlTraveler = require('../controllers/traveler');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// GET travel page
+router.get('/travel', ctrlTraveler.travel);
 
 module.exports = router;
